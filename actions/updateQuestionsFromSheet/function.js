2018be556de5cb64074628b744a03fd3059f2598
Andrew Catton <>
2019-04-17 20:55:19 +0000

    Refactor into sheets-helpers lib

--- actions/updateQuestionsFromSheet/function.js
+++ actions/updateQuestionsFromSheet/function.js
@@ -1,39 +1,17 @@
 function(checklist, sheetId, ellipsis) {
   const skills = require('skills')(ellipsis);
-const client = require('google-client')(ellipsis);
-const {google} = ellipsis.require('googleapis@38.0.0');
-const sheets = google.sheets({
-  version: 'v4',
-  auth: client
-});
+const sheets = require('sheets-helpers')(ellipsis);
 const skillId = checklist.id;
 
-const request = {
-  spreadsheetId: sheetId,
-  ranges: [],
-  includeGridData: false,
-  auth: client
-};
-
-client.authorize().then(() => {
-  const request = {
-    spreadsheetId: sheetId,
-    ranges: ['Sheet1'],
-    auth: client,
-  };
-  sheets.spreadsheets.values.batchGet(request).then(res => {
-    const rows = res.data.valueRanges[0].values;
-    const questions = rows.map(ea => ea[0]);
-
-    skills.replaceQuestions(skillId, questions).then(json => {
-      ellipsis.success('Done!', {
-        next: {
-          actionName: 'displayActions',
-          args: [
-            { name: 'skillId', value: skillId }
-          ]
-        }
-      });
+sheets.getQuestionsIn(sheetId).then(questions => {
+  skills.replaceQuestions(skillId, questions).then(json => {
+    ellipsis.success('Done!', {
+      next: {
+        actionName: 'displayActions',
+        args: [
+          { name: 'skillId', value: skillId }
+        ]
+      }
     });
   });
 });
