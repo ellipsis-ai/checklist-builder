35b71401be58d73b449519fe42628076646ef508
Andrew Catton <>
2019-04-22 15:54:09 +0000

    now give the checklist spreadsheet a name

--- actions/updateQuestionsFromSheet/function.js
+++ actions/updateQuestionsFromSheet/function.js
@@ -2,31 +2,32 @@ function(skillId, ellipsis) {
   const skills = require('skills')(ellipsis);
 const sheets = require('sheets-helpers')(ellipsis);
 
-sheets.create().then(createResult => {
-  const data = createResult.data;
-  skills.getInputs(skillId).then(inputs => {
-    const permalink = ellipsis.event.message ? ellipsis.event.message.permalink : undefined;
-    const heading = 
-      permalink ?
-        `=HYPERLINK("${permalink}", "Edit the questions below. Once you're done, click here to return to Slack and update your checklist")` :
-        `Edit the questions below. Once you're done, return to Slack and update your checklist`;
-    sheets.putQuestions(data.spreadsheetId, inputs.map(ea => ea.question), heading).then(updateResult => {
-      ellipsis.success(data.spreadsheetUrl, {
-        choices: [
-          {
-            actionName: 'pullQuestionsFromSheet',
-            label: 'Update checklist questions',
-            allowMultipleSelections: true,
-            allowOthers: true,
-            args: [
-              { name: 'skillId', value: skillId },
-              { name: 'sheetId', value: data.spreadsheetId }
-            ]
-          }
-        ]
+skills.getNameFor(skillId).then(name => {
+  sheets.create(name).then(createResult => {
+    const data = createResult.data;
+    skills.getInputs(skillId).then(inputs => {
+      const permalink = ellipsis.event.message ? ellipsis.event.message.permalink : undefined;
+      const heading = 
+        permalink ?
+          `=HYPERLINK("${permalink}", "Edit the questions below. Once you're done, click here to return to Slack and update your checklist")` :
+          `Edit the questions below. Once you're done, return to Slack and update your checklist`;
+      sheets.putQuestions(data.spreadsheetId, inputs.map(ea => ea.question), heading).then(updateResult => {
+        ellipsis.success(data.spreadsheetUrl, {
+          choices: [
+            {
+              actionName: 'pullQuestionsFromSheet',
+              label: 'Update checklist questions',
+              allowMultipleSelections: true,
+              allowOthers: true,
+              args: [
+                { name: 'skillId', value: skillId },
+                { name: 'sheetId', value: data.spreadsheetId }
+              ]
+            }
+          ]
+        });
       });
     });
   });
-
-});
+})
 }
